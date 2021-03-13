a96c436e22b0a85d544d6963936de6953aa334f1
NV7150 <7150macNV777DAI@gmail.com>
2021-03-13 15:43:11 +0900

    update: ホーム画面のレイアウト調子え

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -3,15 +3,32 @@ import HomeTop from "../Home/HomeTop";
 import MyData from "../assets/datas/MyData.json"
 import HomeWorks from "../Home/HomeWorks";
 import WorkData from "../assets/datas/WorkData";
+import {createStyles, makeStyles} from "@material-ui/styles";
+import {Box, Theme} from "@material-ui/core";
 
 type HomeProps = {
 
 }
 
+const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+        root: {
+        },
+        columns: {
+            paddingTop: "10vh",
+            marginBottom : "25vh"
+        }
+    })
+
+)
+
 const Home = (props: HomeProps) => {
+    const classes = useStyles();
     return (
         <div>
-            <HomeTop info={MyData} />
+            <Box className={classes.columns}>
+                <HomeTop info={MyData}  />
+            </Box>
             <HomeWorks works={WorkData.works} />
         </div>
     );
